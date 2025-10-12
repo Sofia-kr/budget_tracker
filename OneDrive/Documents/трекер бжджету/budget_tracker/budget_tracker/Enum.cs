648bf003e05f557aa510af3489c5db477c3c791e
Sofia-kr <23krychkovska.s@nltu.lviv.ua>
2025-10-13 00:25:08 +0300

    головний екран

---
+++ OneDrive/Documents/трекер бжджету/budget_tracker/budget_tracker/Enum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budget_tracker
+{
+    public enum ViewType
+    {
+        Expenses,
+        Income,
+        Savings
+    }
+    public enum PeriodType
+    {
+        Day = 0,
+        Week = 1,
+        Month = 2,
+        Year = 3,
+        Custom = 4
+    }
+
+}
---
+++ OneDrive/Documents/трекер бжджету/budget_tracker/budget_tracker/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budget_tracker
+{
+    using System;
+
+   
+        public class Transaction
+        {
+            public int Id { get; set; }
+            public decimal Amount { get; set; }
+            public DateTime Date { get; set; }
+            public string Type { get; set; } // "Expenses", "Income", "Savings"
+            public string Description { get; set; }
+            public string Category { get; set; }
+
+            public Transaction()
+            {
+                Date = DateTime.Now;
+            }
+        
+    }
+}
